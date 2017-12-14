a908e1600cad14c59bedd17939b2ba04a45a8865
Otavio Angeli <otavio.miguel.angeli@gmail.com>
2017-12-14 19:08:44 -0200

    Mudança nos icones do menu lateral

--- app/src/main/java/uniandrade/br/edu/com/popseries/Views/MainActivity.java
+++ app/src/main/java/uniandrade/br/edu/com/popseries/Views/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_serie);
     }
 
     @Override
@@ -73,15 +74,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_serie) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_amigos) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_perfil) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_ajuda) {
 
-        } else if (id == R.id.nav_send) {
+    } else if (id == R.id.nav_sobre) {
 
         }
 
