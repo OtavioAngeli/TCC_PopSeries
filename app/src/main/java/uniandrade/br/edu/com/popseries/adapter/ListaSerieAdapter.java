f7ff85668744dae7a7cfd424e7918cdc657f1f79
Otavio Angeli <otavio.miguel.angeli@gmail.com>
2017-12-14 22:44:43 -0200

    Criado layout base da lista de séries + Adapter da lista de séries

---
+++ app/src/main/java/uniandrade/br/edu/com/popseries/adapter/ListaSerieAdapter.java
@@ -0,0 +1,67 @@
+package uniandrade.br.edu.com.popseries.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import uniandrade.br.edu.com.popseries.R;
+
+/**
+ * Created by pnda on 14/12/17.
+ * Classe adapter para disponibilizar as séries em lista
+ */
+
+public class ListaSerieAdapter extends RecyclerView.Adapter<ListaSerieAdapter.ViewHolder> {
+
+//    private List<SeriesResults.ItemsBean> mSerieList;
+    private Context mContext;
+
+    public ListaSerieAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+
+    @Override
+    public ListaSerieAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.lista_series_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ListaSerieAdapter.ViewHolder holder, int position) {
+        // NOME
+        holder.txtNomeSerie.setText("Braking Bad");
+
+        //DATA LANÇAMENTO
+        holder.txtReleaseDate.setText("Lançamento: Desconhecido");
+    }
+
+    @Override
+    public int getItemCount() {
+//        return (mSerieList == null) ? 0 : mSerieList.size();
+        return 5;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imgCapaSerie;
+        private TextView txtNomeSerie;
+        private TextView txtReleaseDate;
+
+        public ViewHolder(View ItemView){
+            super(ItemView);
+
+            imgCapaSerie = itemView.findViewById(R.id.imgCapaSerie);
+            txtNomeSerie = itemView.findViewById(R.id.txtNomeSerie);
+            txtReleaseDate = itemView.findViewById(R.id.txtReleaseDate);
+
+        }
+    }
+
+}
--- app/src/main/java/uniandrade/br/edu/com/popseries/fragments/SeriesFragment.java
+++ app/src/main/java/uniandrade/br/edu/com/popseries/fragments/SeriesFragment.java
@@ -3,17 +3,23 @@ package uniandrade.br.edu.com.popseries.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import uniandrade.br.edu.com.popseries.R;
+import uniandrade.br.edu.com.popseries.adapter.ListaSerieAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SeriesFragment extends Fragment {
 
+
+    private RecyclerView recyclerView;
+    private ListaSerieAdapter listaSerieAdapter;
 
     public SeriesFragment() {
         // Required empty public constructor
@@ -26,6 +32,13 @@ public class SeriesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_series, container, false);
 
+        recyclerView = view.findViewById(R.id.recyclerView);
+        listaSerieAdapter = new ListaSerieAdapter(getContext());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+
+        recyclerView.setAdapter(listaSerieAdapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
 
         return view;
     }
