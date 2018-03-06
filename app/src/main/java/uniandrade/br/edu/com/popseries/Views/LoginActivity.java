9b55c158ee9d69b90b2ad02677ea228143d4cd54
Otavio Angeli <otavio.miguel.angeli@gmail.com>
2018-03-06 16:45:20 -0300

    configurado interação de login do google com o firebase

--- app/src/main/java/uniandrade/br/edu/com/popseries/Views/LoginActivity.java
+++ app/src/main/java/uniandrade/br/edu/com/popseries/Views/LoginActivity.java
@@ -1,15 +1,155 @@
 package uniandrade.br.edu.com.popseries.Views;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import uniandrade.br.edu.com.popseries.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    Button btnLoginGoogle;
+    GoogleApiClient mGoogleApiClient;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener mFirebaseAuthListener;
+
+    private ProgressBar progressBar;
+
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+
+        btnLoginGoogle = findViewById(R.id.btnLoginGoogle);
+        progressBar = findViewById(R.id.progressBarLoginGoogle);
+
+        btnLoginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        mFirebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null){
+                    goMainScreen();
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(mFirebaseAuthListener);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        goMainScreen();
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult: " + result.isSuccess());
+        if (result.isSuccess()){
+            firebaseAuthWithGoogle(result.getSignInAccount());
+        }else {
+            Toast.makeText(getApplicationContext(),"Erro ao fazer login", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount signInAccount) {
+        progressBar.setVisibility(View.VISIBLE);
+        btnLoginGoogle.setVisibility(View.GONE);
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                progressBar.setVisibility(View.GONE);
+                btnLoginGoogle.setVisibility(View.VISIBLE);
+
+                if (!task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Erro ao autenticar no Firebase", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mFirebaseAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(mFirebaseAuthListener);
+        }
     }
 }
--- app/src/main/java/uniandrade/br/edu/com/popseries/Views/MainActivity.java
+++ app/src/main/java/uniandrade/br/edu/com/popseries/Views/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,67 +14,77 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import uniandrade.br.edu.com.popseries.R;
 import uniandrade.br.edu.com.popseries.fragments.AssistidosFragment;
 import uniandrade.br.edu.com.popseries.fragments.AssistirMaisTardeFragment;
 import uniandrade.br.edu.com.popseries.fragments.FavoritosFragment;
 import uniandrade.br.edu.com.popseries.fragments.SeriesFragment;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private int i;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.content_main, new SeriesFragment()).commit();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setCheckedItem(R.id.nav_serie);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.menu_bottom);
+        final BottomNavigationView bottomNavigationView = findViewById(R.id.menu_bottom);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFragment = null;
-                switch (item.getItemId()){
-                    case R.id.nav_bottom_series:
-                        setTitle(R.string.app_name);
-                        getSupportFragmentManager()
-                                .beginTransaction()
-                                .replace(R.id.content_main, new SeriesFragment()).commit();
-                        break;
-                    case R.id.nav_bottom_favoritos:
-                        setTitle("Favoritos");
-                        getSupportFragmentManager()
-                                .beginTransaction()
-                                .replace(R.id.content_main, new FavoritosFragment()).commit();
-                        break;
-                    case R.id.nav_bottom_assistidos:
-                        setTitle("Assistidos");
-                        getSupportFragmentManager()
-                                .beginTransaction()
-                                .replace(R.id.content_main, new AssistidosFragment()).commit();
-                        break;
-                    case R.id.nav_bottom_favorito4:
-                        setTitle("Assistir Mais Tarde");
-                        getSupportFragmentManager()
-                                .beginTransaction()
-                                .replace(R.id.content_main, new AssistirMaisTardeFragment()).commit();
-                        break;
+                if (item.getItemId() != i){
+                    i = item.getItemId();
+                    switch (item.getItemId()){
+                        case R.id.nav_bottom_series:
+                            item.setEnabled(true);
+                            setTitle(R.string.app_name);
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.content_main, new SeriesFragment()).commit();
+                            break;
+                        case R.id.nav_bottom_favoritos:
+                            setTitle("Favoritos");
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.content_main, new FavoritosFragment()).commit();
+                            break;
+                        case R.id.nav_bottom_assistidos:
+                            setTitle("Assistidos");
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.content_main, new AssistidosFragment()).commit();
+                            break;
+                        case R.id.nav_bottom_favorito4:
+                            setTitle("Assistir Mais Tarde");
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.content_main, new AssistirMaisTardeFragment()).commit();
+                            break;
+                    }
                 }
                 return true;
             }
@@ -83,9 +92,20 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    private boolean verificarUsuarioLogado() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null){
+//            Toast.makeText(getApplicationContext(), "USUARIO LOGADO", Toast.LENGTH_SHORT).show();
+            return true;
+        }else {
+//            Toast.makeText(getApplicationContext(), "USUARIO NÃO LOGADO", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -96,7 +116,11 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        if (verificarUsuarioLogado()){
+            getMenuInflater().inflate(R.menu.menu_user_logado, menu);
+        }else {
+            getMenuInflater().inflate(R.menu.main, menu);
+        }
         return true;
     }
 
@@ -113,9 +137,25 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
         }else if (id == R.id.menu_pesquisa){
            Toast.makeText(getApplicationContext(),"Menu Pesquisa", Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.action_logout){
+            signOut();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void signOut() {
+        firebaseAuth.signOut();
+        Toast.makeText(getApplicationContext(), "Logout realizado com sucesso !", Toast.LENGTH_SHORT).show();
+        verificarUsuarioLogado();
+        goLoginScreen();
+    }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -141,7 +181,7 @@ public class MainActivity extends AppCompatActivity
             Toast.makeText(getApplicationContext(),"Menu Sobre", Toast.LENGTH_SHORT).show();
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
