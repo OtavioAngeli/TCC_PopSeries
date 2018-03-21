4b2cebe51e373e09548935f697db234d68715890
Davi Santos <davi.santosw@gmail.com>
2018-03-21 15:36:04 -0300

    Teste

---
+++ app/src/main/java/uniandrade/br/edu/com/popseries/views/AjudaActivity.java
@@ -0,0 +1,15 @@
+package uniandrade.br.edu.com.popseries.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uniandrade.br.edu.com.popseries.R;
+
+public class AjudaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajuda);
+    }
+}
